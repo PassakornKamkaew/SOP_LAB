6b13b0eae012414cbdcb1496dd700718e0bbbded
Peak1z <88501407+PassakornKamkaew@users.noreply.github.com>
2023-09-19 13:19:09 +0700

    Add files via upload

---
+++ lab6/pojo/Wizards.java
@@ -0,0 +1,13 @@
+package com.example.lab6.pojo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Wizards implements Serializable {
+    ArrayList<Wizard> model = new ArrayList<Wizard>();
+
+    public Wizards(){
+
+    }
+
+}
